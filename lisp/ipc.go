d105657d58a9bf9f53fa7b5909ee0ee55da005da
Dino Farinacci <farinacci@gmail.com>
2018-02-23 15:51:07 -0800

    Commit as of release 0.443.

---
+++ lisp/ipc.go
@@ -0,0 +1,460 @@
+//
+// lisp.go
+//
+// The functions contain in this file are used to interface with the
+// lispers.net control-plane. 
+//
+// This is a external data-plane from the lispers.net control-plane perspective
+// and must be run with "lisp xtr-parameters" sub-command "ipc-data-plane =
+// yes".
+//
+package main
+
+import "fmt"
+import "net"
+import "time"
+import "encoding/json"
+import "os"
+import "strconv"
+
+const lispers_dir = "./"
+
+//
+// 200ms rate-limiter in units of nanaoseconds.
+//
+const lisp_rate_limiter = 200000000
+
+//
+// Sockets used for control-plane IPC.
+//
+var lisp_ipc_socket  *net.UnixConn
+var lisp_punt_socket *net.UnixConn
+var lisp_last_punt   time.Time
+
+//
+// Data structures for running the data-plane.
+//
+var lisp_database     []Lisp_database
+//  lisp_map_cache    []*Lisp_map_cache = lisp_lml_cache
+var lisp_interfaces   map[string]Lisp_interface
+var lisp_etr_nat_port int
+var lisp_decap_keys   map[string]string
+
+//
+// lisp_ipc_message_processing
+//
+// Listen on socket "lisp-ipc-data-plane" for messages from the lispers.net
+// python control-plane.
+//
+func lisp_ipc_message_processing() {
+	var lisp_interface      Lisp_interface
+	var lisp_database_entry Lisp_database
+	var eid                 string
+	var jdata               map[string]interface{}
+	var sa                  net.UnixAddr
+
+	sa.Name = lispers_dir + "lisp-ipc-data-plane"
+	sa.Net = "unixgram"
+
+	//
+	// If named socket file exists, remove it.
+	//
+	_, err := os.Stat(sa.Name)
+	if (err == nil) {
+		os.Remove(sa.Name)
+	}
+
+	socket, err := net.ListenUnixgram("unixgram", &sa)
+	if (err != nil) {
+		lprint("net.ListenUnixgram() failed: %s", err)
+		return
+	}
+	lisp_ipc_socket = socket
+
+	buf := make([]byte, 8192)
+	for {
+		time.Sleep(100 * time.Millisecond)
+
+		n, err := socket.Read(buf[:])
+		if (err != nil) {
+			lprint("socket.Read() failed: %s", err)
+			continue
+		}
+
+		lprint("Received %s: '%s'", bold("IPC"), buf[0:n])
+
+		jdata = make(map[string]interface{}, 0)
+		err = json.Unmarshal(buf[0:n], &jdata)
+		if (err != nil) {
+			lprint("json.Unmarshall() failed: %s", err)
+			continue
+		}
+		value, ok := jdata["type"]
+		if (!ok) {
+			lprint("JSON 'type' not found")
+			continue
+		}
+
+		//
+		// Process each JSON type.
+		//
+		if (value == "entire-map-cache") {
+			for _, jj := range jdata["entries"].([]interface{}) {
+				j := jj.(map[string]interface{})
+				lisp_store_map_cache_data(j)
+			}
+
+		} else if (value == "map-cache") {
+			lisp_store_map_cache_data(jdata)
+
+		} else if (value == "database-mappings") {
+			lisp_database = make([]Lisp_database, 0)
+			for _, jj := range jdata["database-mappings"].([]interface{}) {
+				j := jj.(map[string]interface{})
+				iid, _ := strconv.Atoi(j["instance-id"].(string))
+				eid = j["eid-prefix"].(string)
+				lisp_database_entry.eid_prefix.lisp_store_address(iid, eid)
+				lisp_database = append(lisp_database, lisp_database_entry)
+			}
+			if (len(lisp_database) != 0 && len(lisp_interfaces) != 0) {
+				lisp_start_itr_data_plane()
+			}
+
+		} else if (value == "interfaces") {
+ 			new_interfaces := make(map[string]Lisp_interface, 0)
+			for _, jj := range jdata["interfaces"].([]interface{}) {
+				j := jj.(map[string]interface{})
+				device := j["interface"].(string)
+				iid, _ := strconv.Atoi(j["instance-id"].(string))
+
+				entry, ok := lisp_interfaces[device]
+				if (ok) {
+					entry.instance_id = int(iid)
+					new_interfaces[device] = entry
+				} else {
+					lisp_interface.instance_id = int(iid)
+					lisp_interface.thread_started = false
+					new_interfaces[device] = lisp_interface
+				}
+			}
+			lisp_interfaces = new_interfaces
+			if (len(lisp_database) != 0 && len(lisp_interfaces) != 0) {
+				lisp_start_itr_data_plane()
+			}
+
+		} else if (value == "etr-nat-port") {
+			old_port := lisp_etr_nat_port
+			lisp_etr_nat_port = int(jdata["port"].(float64))
+			if (lisp_etr_nat_port != old_port) {
+				lisp_create_decap_nat_capture()
+			}
+
+		} else if (value == "xtr-parameters") {
+			lisp_debug_logging = jdata["control-plane-logging"].(bool)
+			lisp_data_plane_logging = jdata["data-plane-logging"].(bool)
+
+		} else {
+			lprint("JSON '%s' not supported", value)
+			continue
+		}
+
+		//
+		// Display entire state.
+		//
+		out := lisp_show_state()
+		lisp_write_file("./show-xtr", out)
+		fmt.Printf(out)
+	}
+}
+
+//
+// lisp_store_map_cache_data
+//
+// Store map-cache data from this JSON structure documented in lisp-ipc-data-
+// plane.tx.
+//
+func lisp_store_map_cache_data(jdata map[string]interface{}) {
+	var lisp_mc_entry *Lisp_map_cache
+	var eid           Lisp_address
+	var rloc, rle     Lisp_rloc
+
+	iid, _ := strconv.Atoi(jdata["instance-id"].(string))
+	eid.lisp_store_address(iid, jdata["eid-prefix"].(string))
+	lisp_mc_entry = new(Lisp_map_cache)
+	lisp_mc_entry.eid_prefix = eid
+
+	//
+	// Find entry and remove it. If opcode is add, then append to array.
+	// If opcode is a delete, just return.
+	//
+	mc := lisp_lml_exact_lookup(eid)
+	if (mc != nil) {
+		lisp_lml_delete_entry(mc)
+	}
+	if (jdata["opcode"] == "add") {
+		rloc_set := jdata["rlocs"]
+		rle_set := jdata["rles"]
+
+		if (rloc_set != nil) {
+			for _, jj := range rloc_set.([]interface{}) {
+				j := jj.(map[string]interface{})
+				rloc.rloc.lisp_store_address(0, j["rloc"].(string))
+				rloc.encap_port, _ = strconv.Atoi(j["port"].(string))
+				_, ok := j["encrypt-key"].(string)
+				if (ok) { rloc.encrypt_key = j["encrypt-key"].(string) }
+				_, ok = j["icv-key"].(string)
+				if (ok) { rloc.icv_key = j["icv-key"].(string) }
+				lisp_mc_entry.rloc_set = append(lisp_mc_entry.rloc_set, rloc)
+			}
+		}
+		if (rle_set != nil) {
+			for _, jj := range rle_set.([]interface{}) {
+				j := jj.(map[string]interface{})
+				rle.rloc.lisp_store_address(0, j["rle"].(string))
+				rle.encap_port, _ = strconv.Atoi(j["port"].(string))
+				_, ok := j["encrypt-key"].(string)
+				if (ok) { rle.encrypt_key = j["encrypt-key"].(string) }
+				_, ok = j["icv-key"].(string)
+				if (ok) { rle.icv_key = j["icv-key"].(string) }
+				lisp_mc_entry.rle_set = append(lisp_mc_entry.rle_set, rle)
+			}
+ 		}
+		lisp_lml_add_entry(lisp_mc_entry)
+	}
+}
+
+//
+// lisp_show_state
+//
+// Show data structure state.
+//
+func lisp_show_state() string {
+	out := "lispers.net release " + bold(lisp_read_file("./lisp-version.txt"))
+	out += " running at " + lisp_command_output("date") + "\n\n"
+	out += fmt.Sprintf("%s\n", bold("LISP xTR State"))
+	out += fmt.Sprintf("  LISP ETR NAT Port: %d\n", lisp_etr_nat_port)
+	out += fmt.Sprintf("  LISP Interfaces: ")
+	for key, value := range lisp_interfaces {
+		out += fmt.Sprintf("%s:[%d] ", key, value.instance_id)
+	}
+	out += fmt.Sprintf("\n")
+ 
+	out += fmt.Sprintf("  LISP Databaase Mappings: ")
+	for i, value := range lisp_database {
+		out += fmt.Sprintf("%s", value.eid_prefix.lisp_print_address(true))
+		if (i == len(lisp_database)-1) {
+			out += fmt.Sprintf("\n")
+		} else {
+			out += fmt.Sprintf(", ")
+		}
+	}
+	out += fmt.Sprintf("\n")
+
+	out += fmt.Sprintf("%s\n", bold("LISP xTR Map-Cache State"))
+	for mc := lisp_lml_walk(nil); mc != nil; mc = lisp_lml_walk(mc) {
+		if (len(mc.rloc_set) == 0 && len(mc.rle_set) == 0) {
+			out += fmt.Sprintf("  EID: %s, rloc-set: [], rle-set: []\n",
+				mc.eid_prefix.lisp_print_address(true))
+			continue
+		}
+		if (len(mc.rloc_set) != 0) {
+			out += fmt.Sprintf("  EID: %s, rloc-set: ",
+				mc.eid_prefix.lisp_print_address(true))
+			for i, rloc := range mc.rloc_set {
+				out += fmt.Sprintf("%s:%d",
+					rloc.rloc.lisp_print_address(false), rloc.encap_port)
+				if (i != len(mc.rloc_set)-1) {
+					out += fmt.Sprintf(", ")
+				}
+			}
+			out += fmt.Sprintf("\n")
+		}
+		if (len(mc.rle_set) != 0) {
+			out += fmt.Sprintf("  EID: %s, rle-set: ",
+				mc.eid_prefix.lisp_print_address(true))
+			for i, rle := range mc.rle_set {
+				out += fmt.Sprintf("%s:%d", rle.rloc.lisp_print_address(false),
+					rle.encap_port)
+				if (i != len(mc.rle_set)-1) {
+					out += fmt.Sprintf(", ")
+				}
+			}
+			out += fmt.Sprintf("\n")
+		}
+	}
+	out += fmt.Sprintf("\n")
+	return(out)
+}
+
+//
+// lisp_create_punt_socket
+//
+// Create named socket 'lispers.net-itr: is lispers.net directory.
+//
+func lisp_create_punt_socket() bool {
+	var sa    net.UnixAddr
+	var found error
+
+	sa.Name = lispers_dir + "lispers.net-itr"
+	sa.Net = "unixgram"
+
+	for i := 0; i < 4; i++ {
+		_, found = os.Stat(sa.Name)
+		if (found == nil) {	break }
+		lprint("Punt socket %s does not exist, waiting ...", sa.Name)
+		time.Sleep(time.Duration(i) * (time.Second * 2))
+	}
+	if (found != nil) { return(false) }
+ 	lprint("Punt socket %s found", sa.Name)
+
+	socket, err := net.DialUnix("unixgram", nil, &sa)
+	if (err != nil) {
+		lprint("net.DialUnix() failed: %s", err)
+		return(false)
+	}
+	lisp_punt_socket = socket
+
+	//
+	// Tell control-plane we have restarted.
+	//
+	lisp_send_restart()
+	return(true)
+}
+
+//
+// lisp_punt_packet
+//
+// Send IPC message to punt packet.
+//
+func lisp_punt_packet(input_interface string, seid Lisp_address,
+	deid Lisp_address) {
+	var ipc = map[string]string{ "type" : "discovery", "source-eid" : "",
+		"dest-eid" : "", "interface" : "", "instance-id" : "" }
+
+	//
+	// Check rate-limiter before bothering the control-plane.
+	//
+	elapsed := time.Since(lisp_last_punt).Nanoseconds()
+	if (elapsed <= lisp_rate_limiter) {
+		s := green(seid.lisp_print_address(false))
+		d := green(deid.lisp_print_address(false))
+		dprint("Rate-limit punt packet %s -> %s", s, d)
+		return
+	}
+
+	if (input_interface == "?") {
+		iid := string(seid.instance_id)
+		if (seid.instance_id == 0xffffff) {
+			iid = "-1"
+		}
+		ipc["instance-id"] = iid
+	} else {
+		ipc["interface"] = input_interface
+		iid := lisp_interfaces[input_interface].instance_id
+		ipc["instance-id"] = string(iid)
+	}
+	ipc["source-eid"] = seid.lisp_print_address(false)
+	ipc["dest-eid"] = deid.lisp_print_address(false)
+
+	//
+	// Encode in JSON and send on lispers.net-itr named socket.
+	//
+	jdata, err := json.Marshal(ipc)
+	if (err != nil) {
+		lprint("json.Marshal() failed: %s", err)
+		return
+	}
+
+	lprint("Send %s: '%s'", bold("IPC"), jdata)
+	lisp_punt_socket.Write(jdata)
+	lisp_last_punt = time.Now()
+}
+
+//
+// lisp_send_restart
+//
+// Send IPC message to control-plane indicating that this data-plane has restarted.
+//
+func lisp_send_restart() {
+	var ipc = map[string]string{ "type" : "restart" }
+
+	//
+	// Encode in JSON and send on lispers.net-itr named socket.
+	//
+	jdata, err := json.Marshal(ipc)
+	if (err != nil) {
+		lprint("json.Marshal() failed: %s", err)
+		return
+	}
+
+	lprint("Send %s: '%s'", bold("IPC"), jdata)
+	lisp_punt_socket.Write(jdata)
+}
+
+//
+// lisp_stats_thread
+//
+// Peridoically send data-plane stats to the lispers.net control-plane.
+//
+func lisp_stats_thread() {
+	print_idle := false
+	ipc := make(map[string]interface{}, 0)
+	ipc_eid := make(map[string]interface{}, 0)
+	ipc_rloc := make(map[string]interface{}, 0)
+
+	for {
+		eids := make([]interface{}, 0)
+		count := 0
+		for mc := lisp_lml_walk(nil); mc != nil; mc = lisp_lml_walk(mc) {
+			count += 1
+			rlocs := make([]interface{}, 0)
+			for j, _ := range mc.rloc_set {
+				rloc := &mc.rloc_set[j]
+				if (rloc.packets == rloc.last_reported_packets) {
+					continue
+				}
+				ipc_rloc["rloc"] = rloc.rloc.lisp_print_address(false)
+				ipc_rloc["port"] = fmt.Sprintf("%d", rloc.encap_port)
+				ipc_rloc["packet-count"] = rloc.packets
+				ipc_rloc["byte-count"] = rloc.bytes
+				ipc_rloc["seconds-last-packet"] =
+					time.Since(rloc.last_packet).Seconds()
+				rloc.last_reported_packets = rloc.packets
+				rlocs = append(rlocs, ipc_rloc)
+			}
+			if (len(rlocs) == 0) {
+				continue
+			}
+
+			ipc_eid["instance-id"] =
+				fmt.Sprintf("%d", mc.eid_prefix.instance_id)
+			ipc_eid["eid-prefix"] = mc.eid_prefix.lisp_print_address(false)
+			ipc_eid["rlocs"] = rlocs
+			eids = append(eids, ipc_eid)
+		}
+
+		if (len(eids) != 0) {
+			ipc["type"] = "statistics"
+			ipc["entries"] = eids
+
+			//
+			// Encode in JSON and send on lispers.net-itr named socket.
+			//
+			jdata, err := json.Marshal(ipc)
+			if (err == nil) {
+				lprint("Send %s: '%s'", bold("IPC"), jdata)
+				lisp_punt_socket.Write(jdata)
+			} else {
+				lprint("json.Marshal() for stats messsage failed: %s", err)
+			}
+		} else if (print_idle) {
+			lprint("No change for %d map-cache entries, stats message " +
+				"suppressed", count)
+		}
+
+		//
+		// Send stats in 5 seconds if there was any change.
+		//
+		time.Sleep(5 * time.Second)
+	}
+}
---
+++ lisp/lisp.go
@@ -0,0 +1,439 @@
+//
+// lisp.go
+//
+// This file contains function and type definitions used by xtr.go and ipc.go.
+//
+// This is a external data-plane from the lispers.net control-plane perspective
+// and must be run with "lisp xtr-parameters" sub-command "ipc-data-plane =
+// yes".
+//
+package main
+
+import "fmt"
+import "bufio"
+import "os"
+import "os/exec"
+import "strings"
+import "strconv"
+import "time"
+import "net"
+
+//
+// ---------- Variable Definitions ----------
+//
+var lisp_debug_logging      bool = true
+var lisp_data_plane_logging bool = false
+
+//
+// ---------- Constants Definitions ----------
+//
+const LISP_DATA_PORT       = 4341
+const LISP_CTRL_PORT       = 4342
+const LISP_L2_DATA_PORT    = 8472
+const LISP_VXLAN_DATA_PORT = 4789
+const LISP_VXLAN_GPE_PORT  = 4790
+
+//
+// ---------- Type Definitions ----------
+//
+type Lisp_address struct {
+	instance_id        int
+	mask_len           int
+	address            net.IP
+	mask_address       net.IPMask
+	address_string     string
+}
+
+//
+// lisp_print_address
+//
+// Return string with address. And optionally prepend "[<iid>]"
+//
+func (a *Lisp_address) lisp_print_address(with_iid bool) string {
+	if (with_iid) {
+		iid := a.instance_id
+		if (iid == 0xffffff) { iid = -1 }
+		return(fmt.Sprintf("[%d]%s", iid, a.address_string))
+	}
+	return(a.address_string)
+}
+
+//
+// lisp_store_address
+//
+// Store and instance-ID and string representation of an IPv4 or IPv6 address
+// and store in Lisp_address format.
+//
+func (a *Lisp_address) lisp_store_address(iid int, addr string) bool {
+	var address string
+
+	//
+	// Is this address string an address or a prefix?
+	//
+	if (strings.Contains(addr, "/")) {
+		split := strings.Split(addr, "/")
+		address = split[0]
+		a.mask_len, _ = strconv.Atoi(split[1])
+	} else {
+		address = addr
+		a.mask_len = -1
+	}
+	a.instance_id = iid
+
+	//
+	// Parse address string. ParseIP() will put IPv4 addresses in a 16-byte
+	// array. We don't want that because address []byte length will determine
+	// address family.
+	//
+	a.address = net.ParseIP(address)
+	if (strings.Contains(addr, ".")) {
+		a.address = a.address[12:16]
+	}
+
+	//
+	// Set mask-length and mask address.
+	//
+	if (a.mask_len == -1) {
+		a.mask_len = len(a.address) * 8
+	}
+	a.mask_address = net.CIDRMask(a.mask_len, len(a.address) * 8)
+
+	//
+	// Store string for printing.
+	//
+	a.address_string = addr
+	return(true)
+}
+
+//
+// lisp_is_ipv4
+//
+// Return true if Lisp_address is IPv4.
+//
+func (a *Lisp_address) lisp_is_ipv4() bool {
+	return((len(a.address) == 4))
+}
+
+//
+// lisp_is_ipv6
+//
+// Return true if Lisp_address is IPv6.
+//
+func (a *Lisp_address) lisp_is_ipv6() bool {
+	return((len(a.address) == 16))
+}
+
+//
+// lisp_is_multicst
+//
+// Return true if Lisp_address is an IPv4 or IPv6 multicast group address.
+//
+func (a *Lisp_address) lisp_is_multicast() bool {
+	if (a.lisp_is_ipv4()) {
+		return(int(a.address[0]) >= 224 && int(a.address[0]) < 240)
+	}
+	if (a.lisp_is_ipv6()) {
+		return(a.address[0] == 0xff)
+	}
+	return(false)
+}
+
+//
+// lisp_make_address
+//
+// Store and instance-ID and byte representation of an IPv4 or IPv6 address
+// and store in Lisp_address format.
+// 
+func (a *Lisp_address) lisp_make_address(iid int, addr []byte) {
+	a.instance_id = iid
+	a.address = addr
+	a.mask_len = len(a.address) * 8
+	a.mask_address = net.CIDRMask(a.mask_len, len(a.address) * 8)
+	a.address_string = a.address.String()
+}
+
+//
+// lisp_exact_match
+//
+// Compare two addresses and return true if they match.
+//
+func (a *Lisp_address) lisp_exact_match(addr Lisp_address) (bool) {
+	if (len(a.address) != len(addr.address)) {
+		return(false)
+	}
+	if (a.mask_len != addr.mask_len) {
+		return(false)
+	}
+	if (a.instance_id != addr.instance_id) {
+		return(false)
+	}
+	if (a.address.Equal(addr.address) == false) {
+		return(false)
+	}
+	return(true)
+}
+
+//
+// lisp_more_specific
+//
+// Return true if the supplied address is more specific than the method
+// address. If the mask-lengths are the same, a true is returned.
+//
+func (a *Lisp_address) lisp_more_specific(addr Lisp_address) (bool) {
+	if (len(a.address) != len(addr.address)) {
+		return(false)
+	}
+	if (a.instance_id != addr.instance_id) {
+		return(false)
+	}
+	if (a.mask_len > addr.mask_len) {
+		return(false)
+	}
+	for i := 0; i < len(a.address); i++ {
+		if (a.mask_address[i] == 0) {
+			break
+		}
+		if ((a.address[i] & a.mask_address[i]) !=
+			(addr.address[i] & a.mask_address[i])) {
+			return(false)
+		}
+	}
+	return(true)
+}
+
+//
+// lisp_hash_address
+//
+// Hash address to aid in selecting a source UDP port.
+//
+func (a *Lisp_address) lisp_hash_address() uint16 {
+	var hash uint = 0
+
+	for i := 0; i < len(a.address); i++ {
+		hash = hash ^ uint(a.address[i])
+	}
+
+	//
+	// Fold result into a short.
+	//
+	return(uint16(hash >> 16) ^ uint16(hash & 0xffff))
+}
+
+type Lisp_database struct {
+	eid_prefix Lisp_address
+}
+type Lisp_interface struct {
+	instance_id    int
+	thread_started bool
+}
+type Lisp_map_cache struct {
+	next_mc    *Lisp_map_cache
+	eid_prefix Lisp_address
+	rloc_set   []Lisp_rloc
+	rle_set    []Lisp_rloc
+}
+type Lisp_rloc struct {
+	rloc                  Lisp_address
+	encap_port            int
+	encrypt_key           string
+	icv_key               string
+	packets               uint
+	bytes                 uint
+	last_packet           time.Time
+	last_reported_packets uint
+}
+
+//
+// lprint
+//
+// Print control-plane debug logging output when configured.
+//
+func lprint(format string, args ...interface{}) {
+	if (!lisp_debug_logging) {
+		return
+	}
+
+	ts := time.Now()
+	ms := ts.Nanosecond() / 1000000
+	ds := fmt.Sprintf("%02d/%02d/%02d %02d:%02d:%02d.%03d", ts.Month(),
+		ts.Day(), ts.Year(), ts.Hour(), ts.Minute(), ts.Second(), ms)
+	f := ds + ": xtr: " + format + "\n"
+	fmt.Printf(f, args...)
+}
+
+//
+// dprint
+//
+// Print data-plane debug logging output when configured.
+//
+func dprint(format string, args ...interface{}) {
+	if (!lisp_data_plane_logging) {
+		return
+	}
+
+	ts := time.Now()
+	ms := ts.Nanosecond() / 1000000
+	ds := fmt.Sprintf("%02d/%02d/%02d %02d:%02d:%02d.%03d", ts.Month(),
+		ts.Day(), ts.Year(), ts.Hour(), ts.Minute(), ts.Second(), ms)
+	f := ds + ": xtr: " + format + "\n"
+	fmt.Printf(f, args...)
+}
+
+//
+// debug
+//
+// For temporary debug output that highlights line in boldface red.
+//
+func debug(format string, args ...interface{}) {
+	f := red(">>>") + format + red("<<<") + "\n"
+	fmt.Printf(f, args...)
+}
+
+//
+// debugv
+//
+// For temporary debug output that shows the contents of a data structure.
+// Very useful for debugging.
+//
+func debugv(args interface{}) {
+	debug("%#v", args)
+}
+
+//
+// lisp_command_output
+//
+// Execute a system command and return a string with output.
+//
+func lisp_command_output(command string) string {
+	cmd := exec.Command(command)
+	out, err := cmd.CombinedOutput()
+	if (err != nil) {
+		return("")
+	}
+	output := string(out)
+	return(output[0:len(output)-1])
+}
+
+//
+// lisp_read_file
+//
+// Read entire file into a string.
+//
+func lisp_read_file(filename string) string {
+	fd, err := os.Open(filename)
+	if (err != nil) {
+		return("")
+	}
+	scanner := bufio.NewScanner(fd)
+	scanner.Scan()
+	fd.Close()
+	return(scanner.Text())
+}
+
+//
+// lisp_write_file
+//
+// Write supplied string to supplied file.
+//
+func lisp_write_file(filename string, text string) {
+	fd, err := os.Create(filename)
+	if (err != nil) {
+		lprint("Could not create file %s", filename)
+		return
+	}
+	_, err = fd.WriteString(text)
+	if (err != nil) {
+		lprint("Could not write string to file %s", filename)
+		return
+	}
+	fd.Close()
+}
+
+//
+// bold
+//
+// Make input string boldface.
+//
+func bold(str string) string {
+    return("\033[1m" + str + "\033[0m")
+}
+
+//
+// green
+//
+// Make input string green.
+//
+func green(str string) string {
+    return("\033[92m" + bold(str) + "\033[0m")
+}
+
+//
+// red
+//
+// Make input string red.
+//
+func red(str string) string {
+    return("\033[91m" + bold(str) + "\033[0m")
+}
+
+//
+// lisp_log_packet
+//
+// Log a received data packet either native or LISP encapsulated.
+//
+func lisp_log_packet(prefix_string string, packet []byte, is_lisp bool) {
+	var num       int
+	var udp, lisp []byte
+
+	if (!lisp_data_plane_logging) {
+		return
+	}
+
+	ip := true
+	if (packet[0] == 0x45) {
+		num = 20
+	} else if (packet[0] == 0x60) {
+		num = 40
+	} else {
+		num = 8
+		if (packet[8] == 0x45) { num += 20 }
+		if (packet[8] == 0x60) { num += 40 }
+		ip = false
+	}
+	udp = packet[num:num+8]
+	lisp = packet[num+8:num+16]
+
+	packet_string := fmt.Sprintf("%s: ", prefix_string)
+	p := packet
+	for i := 0; i < num; i += 4 {
+		packet_string += fmt.Sprintf("%02x%02x%02x%02x ", p[i], p[i+1],
+			p[i+2], p[i+3])
+	}
+
+	//
+	// Return for invalid packet.
+	//
+	if (ip == false) {
+		dprint(packet_string)
+		return
+	}
+
+	if (!is_lisp) {
+		dprint(packet_string)
+		return
+	}
+
+	packet_string += fmt.Sprintf("UDP: ")
+	for i := 0; i < 8; i += 4 {
+		packet_string += fmt.Sprintf("%02x%02x%02x%02x ", udp[i], udp[i+1],
+			udp[i+2], udp[i+3])
+	}
+	packet_string += fmt.Sprintf("LISP: ")
+	for i := 0; i < 8; i += 4 {
+		packet_string += fmt.Sprintf("%02x%02x%02x%02x ", lisp[i], lisp[i+1],
+			lisp[i+2], lisp[i+3])
+	}
+	dprint(packet_string)
+}
+
+//-----------------------------------------------------------------------------
