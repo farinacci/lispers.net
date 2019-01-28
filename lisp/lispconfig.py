d36fa4daab602e8747d58b333f8e2fe1758b1a31
Dino Farinacci <farinacci@gmail.com>
2019-01-27 16:36:52 -0800

    Put copyright/license in all lisp/ and build/ source files.

--- lisp/lispconfig.py
+++ lisp/lispconfig.py
@@ -1,10 +1,28 @@
+# -----------------------------------------------------------------------------
+#             
+# Copyright 2013-2019 lispers.net - Dino Farinacci <farinacci@gmail.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.    
+# 
+# -----------------------------------------------------------------------------
 #
 # lispconfig.py
 #
 # This file contains all configuration support for the LISP subsystem. That
 # includes lisp.config file processing and the RESTful interface via the
 # bottle module.
-#
+# 
+# -----------------------------------------------------------------------------
 
 import lisp
 import os
--- lisp/xtr.go
+++ lisp/xtr.go
@@ -1,3 +1,20 @@
+// ---------------------------------------------------------------------------
+//             
+// Copyright 2013-2019 lispers.net - Dino Farinacci <farinacci@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.    
+// 
+// ---------------------------------------------------------------------------
 //
 // xtr.go
 //
@@ -15,7 +32,9 @@
 // (3) Look at alternative to gopacket.NewPacketSource(). Gopi says use
 //     static buffer. GC will kill you.
 // (4) Fix decap forwarding for IPv6 EIDs. Test IPv6 RLOCs.
-//
+// 
+// ---------------------------------------------------------------------------
+
 package main
 
 import "fmt"
