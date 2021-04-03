b57ddbefc8c2a848d778f00c607f9263f96885f7
Dino Farinacci <farinacci@gmail.com>
2021-04-02 19:04:05 -0700

    o Create make-provision-release.py that takes a tarball release and adds   customized xTR provisioned files and parameters to the tarball. So all the   users has to do is untar and RL. You cannot customize the map-server yet but   can customize the device used for Macos or Docker and the instance-ID.

---
+++ build/make-provision-release.py
@@ -0,0 +1,121 @@
+# -----------------------------------------------------------------------------
+#             
+# Copyright 2013-2019 lispers.net - Dino Farinacci <farinacci@gmail.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.    
+# 
+# -----------------------------------------------------------------------------
+#
+# make-provision-release.py
+#
+# This script takes a lispers.net tgz file and adds lisp.config.provision-xtr,
+# provision.py and RL.provision-xtr files to it so the tgz can be provisioned
+# for a particular instance-ID and mapping system.
+#
+# Usage: python make-provision-release.py [<release> <iid>]
+#
+# -----------------------------------------------------------------------------
+
+import os
+import commands
+import sys
+import time
+import platform
+
+#------------------------------------------------------------------------------
+
+
+#
+# First check that this is running in the build directory.
+#
+curdir = commands.getoutput("pwd")
+curdir = curdir.split("/")
+if (curdir[-1] != "build"):
+    print "Need to be in directory named 'build'"
+    exit(1)
+#endif
+if (os.path.exists("./releases") == False):
+    print "Directory './releases' needs to be in build directory"
+    exit(1)
+#endif
+
+#
+# Get input parameters.
+#
+if (len(sys.argv) == 3): 
+    version = sys.argv[1]
+    iid = sys.argv[2]
+else:
+    version = raw_input("Enter version number (in format x.y): ")
+    iid = raw_input("Enter LISP xTR instance-ID: ")
+#endif
+macos = raw_input("Is this release for MacOS? (y/n): ")
+
+release = "./releases/release-{}".format(version)
+if (os.path.exists(release) == False):
+    print "Could not find directory {}".format(release)
+    exit(1)
+#endif
+tgz = "lispers.net-x86-release-{}.tgz".format(version)
+tmp = "/tmp/{}".format(tgz)
+
+#
+# Make /tmp directory to untar release so we can combine provision files
+# with a "provisioned release" tarball.
+#
+print("Copying provision files with tarball files for release {} ...". \
+      format(version)),
+os.system("mkdir -p {}; cp {}/{} {}".format(tmp, release, tgz, tmp))
+os.system("chmod 755 {}/{}".format(tmp, tgz))
+os.system("cd {}; tar zxf {}; rm {}".format(tmp, tgz, tgz))
+os.system("cp lisp.config.provision-xtr provision-lisp.py {}".format(tmp))
+os.system("cp RL.provision-xtr {}/RL".format(tmp))
+print("done")
+
+#
+# Don't need RL-template in this release since we customize the RL file for
+# the instance-ID and MacOS device (or eth0 for docker).
+#
+print("Customize RL.provision-xtr file with provisioning parameters ..."),
+f = open("{}/RL".format(tmp), "r"); buf = f.read(); f.close()
+buf = buf.replace('set DEVICE = "eth0"', 'set DEVICE = "en0"')
+buf = buf.replace('set IID = "0"', 'set IID = "{}"'.format(iid))
+f = open("{}/RL".format(tmp), "w"); f.write(buf); f.close()
+os.system("rm {}/RL-template".format(tmp))
+print("done")      
+
+#
+# Create new "provisioned release" tarball.
+#
+md = "macos" if (macos) else "docker"
+ptgz = "lispers.net-x86-release-{}-iid-{}-{}.tgz".format(version, iid, md)
+print("Creating provisoined tarball for release {}, instance-ID {} ...". \
+      format(version, iid)),
+cmd = "cd {}; export COPYFILE_DISABLE=true; tar czf {} *".format(tmp, ptgz)
+os.system(cmd)
+print("done")      
+
+#
+# Copy to release directory where tarball of standard release resides.
+#      
+os.system("cp {}/{} {}/{}".format(tmp, ptgz, release, ptgz))
+os.system("rm -fr {}".format(tmp))
+
+#
+# We are done. Tell user where it is.
+#
+print("New file {} written to releases/release-{} directory".format( \
+    ptgz, version))
+exit(0)
+
+#------------------------------------------------------------------------------
