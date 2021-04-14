2ec04d6d8294a53cdf72c567bc45fddc2885ef61
Dino Farinacci <farinacci@gmail.com>
2021-04-14 15:11:02 -0700

    Commit build release release-0.558

---
+++ build/releases/release-0.558/src/is-lisp-running.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#             
+# Copyright 2013-2019 lispers.net - Dino Farinacci <farinacci@gmail.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.    
+# 
+# -----------------------------------------------------------------------------
+#
+# is-lisp-running.py
+#
+# This script checks to see if there is any LISP processes running on the
+# local system.
+#
+# Usage: python -O is-lisp-running.pyo
+#
+#------------------------------------------------------------------------------
+
+import os
+try:
+    from commands import getoutput
+except:
+    from subprocess import getoutput
+#entry    
+
+def bold(string):
+    return("\033[1m" + string + "\033[0m")
+#enddef
+
+#
+# Add title.
+#
+version = getoutput("cat lisp-version.txt")
+rv = getoutput("head -1 logs/lisp-core.log")
+if (rv.find("version") != -1):
+    rv = rv.split("version ")[1]
+    rv = rv.split(",")[0]
+    rv = ", release {} running".format(bold(rv))
+else:
+    rv = None
+#endif
+
+#
+# Get data.
+#
+command = "ps auxww | egrep 'lisp-' |" + \
+    "egrep -v 'grep|is-lisp-running|tee|pslisp|sudo|log'"
+output = getoutput(command)
+if (output == None or output == ""):
+    print("No lispers.net code running, release {} installed".format(version))
+    exit(0)
+#endif
+if (rv == None): rv = ""
+
+print("--- lispers.net release {} installed{} ---".format(bold(version), rv))
+
+lines = output.split("\n")
+
+#
+# Special case Alpine Linux. It has different ps output.
+#
+if (os.path.exists("/etc/alpine-release")):
+    pid = "PID".ljust(8)
+    cpu = "TIME".ljust(8)
+    process = "Process"
+    print("{}{}{}".format(pid, cpu, process))
+    for line in lines:
+        items = line.split()
+        pid = items[0].ljust(8)
+        cpu = items[2].ljust(8)
+        process = items[3] if (items[3].find("lisp-ztr") != -1) else items[-1]
+        if (process.isdigit()): process = items[-2] + " " + process
+        print("{}{}{}".format(pid, cpu, process))
+    #endfor
+    exit(0)
+#endif
+
+#
+# All other Linux variants.
+#
+pid = "PID".ljust(8)
+cpu = "%CPU".ljust(8)
+mem = "%MEM".ljust(8)
+mb = "MEM".ljust(8)
+process = "Process"
+print("{}{}{}{}{}".format(pid, cpu, mem, mb, process))
+
+for line in lines:
+    items = line.split()
+    pid = items[1].ljust(8)
+    cpu = items[2].ljust(8)
+    mem = items[3].ljust(8)
+
+    mb = int(items[4]) / 1000
+    gm = "M"
+    if (mb >= 1000):
+        mb = round(float(mb) / 1000, 1)
+        gm = "G"
+    #endif
+    mb = "{}{}".format(mb, gm).ljust(8)
+
+    if (line.find("lisp-core.py") != -1):
+        process = items[-2] + " " + items[-1]
+    elif (line.find("lisp-join.py") != -1):
+        group = items[-1]
+        if (group.count(".") != 3): group = items[-2] + " " + group
+        process = "lisp-join.py " + group
+    else:
+        process = items[-1]
+    #endif
+
+    print("{}{}{}{}{}".format(pid, cpu, mem, mb, process))
+#endfor
+exit(0)
---
+++ build/releases/release-0.558/src/lisp-log-packets.py
@@ -0,0 +1,124 @@
+# -----------------------------------------------------------------------------
+#             
+# Copyright 2013-2019 lispers.net - Dino Farinacci <farinacci@gmail.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.    
+# 
+# -----------------------------------------------------------------------------
+#
+# lisp-log-packets.py
+#
+# Set the following commands in the local system:
+# 
+# lisp debug {
+#     itr = yes
+#     etr = yes
+# }
+# lisp xtr-parameters {
+#     data-plane-logging = yes
+# }
+#
+# By using the lispapi. The command will toggle the setting. When environment
+# variable LISP_PW is set, the value is the password that is used to connect.
+# to the LISP API.
+#
+# Usage: python -O lisp-log-packets.py [force] [<api-port>] [help]
+#
+# Note when <api-port> is negative, it is passed into the lispapi to tell it
+# to use http versus https. This port number should be the same value as
+# passed on the ./RESTART-LISP command.
+#
+#------------------------------------------------------------------------------
+
+import lispapi
+import sys
+import os
+
+#
+# Get command-line parameters.
+#
+force = True
+api_port = 8080
+for arg in sys.argv[1::]:
+    if (arg == "help"):
+        print("Usage: log-packets [force] [<api-port>] [help]")
+        exit(0)
+    #endif
+    if (arg == "force"): force = True
+    if (arg.isdigit()): api_port = int(arg)
+    if (arg[0] == "-" and arg[1::].isdigit()): api_port = -int(arg[1::])
+#endfor
+
+#
+# Get user supplied password, if any.
+#
+pw = os.getenv("LISP_PW")
+if (pw == None): pw = ""
+
+#
+# Open API to localhost. If debug status dict array None, the open failed.
+#
+lisp = lispapi.api_init("localhost", "root", pw, port=api_port)
+if (lisp.debug_status == None):
+    print ("Could not connect to API, is lispers.net running?, " + \
+        "LISP_PW set?, or using the correct port number?")
+    exit(1)
+#endif
+
+#
+# Get current settings for control-plane.
+#
+itr = lisp.is_itr_debug_enabled()
+etr = lisp.is_etr_debug_enabled()
+rtr = lisp.is_rtr_debug_enabled()
+
+#
+# Get current settings for data-plane logging.
+#
+xtr_parms = lisp.get_xtr_parameters()
+if (xtr_parms == None):
+    print("Could not get xtr-parameters from API")
+    exit(1)
+#endif
+
+dp_logging = xtr_parms["data-plane-logging"] == "yes"
+rtr_running = lisp.is_rtr_enabled()
+
+if (dp_logging):
+    lisp.disable_xtr_data_plane_logging()
+    lisp.disable_itr_debug()
+    lisp.disable_etr_debug()
+    if (rtr_running): lisp.disable_rtr_debug()
+    print("Data-plane logging has been disabled")
+else:
+    if (rtr_running):
+        if (rtr and force == False):
+            print("Control-plane logging is enabled, no action taken")
+            exit(0)
+        #endif
+        lisp.enable_rtr_debug()
+    else:
+        if ((itr or etr) and force == False):
+            print("Control-plane logging is enabled, no action taken")
+            exit(0)
+        #endif
+        lisp.enable_itr_debug()
+        lisp.enable_etr_debug()
+    #endif
+    lisp.enable_xtr_data_plane_logging()
+    print("Data-plane logging has been enabled")
+#endif
+
+exit(0)
+
+#------------------------------------------------------------------------------
