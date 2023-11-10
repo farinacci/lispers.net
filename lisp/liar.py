cf8f06061f73184dc2a9df1a2f207d8b83ab984a
Dino Farinacci <farinacci@gmail.com>
2023-11-10 18:25:19 +0000

    Complete first phase of liar. Send partial tarball to testers to try out.

--- lisp/liar.py
+++ lisp/liar.py
@@ -31,6 +31,8 @@ import socket
 import struct
 import random
 import binascii
+import hmac
+import hashlib
 
 #------------------------------------------------------------------------------
 
@@ -39,6 +41,7 @@ usage = "Usage: python3 liar.py [-quic] <eid> <rloc> <ms> <ms-key>"
 IPV4_AFI = 1
 IPV6_AFI = 2
 NAME_AFI = 17
+LCAF_AFI = 16387
 
 #------------------------------------------------------------------------------
 
@@ -65,7 +68,7 @@ def main():
     #endif
 
     orig_eid = sys.argv[1]
-    afe, eid, ml = validate_eid(orig_eid)
+    afe, iid, eid, ml = validate_eid(orig_eid)
     if (afe == None): return
 
     orig_rloc = sys.argv[2]
@@ -87,16 +90,16 @@ def main():
     # Build and send a Map-Register. And for now, just exit. One message
     # will be sent.
     #
-    pkt, nonce = build_map_register(afe, eid, ml, afl, rloc, ms, ms_key)
+    pkt, nonce = build_map_register(afe, iid, eid, ml, afl, rloc, ms, ms_key)
 
     #
     # Send on socket.
     #
-#   s.sendto(pkt.encode(), (ms, 4342))
+    s.sendto(pkt, (ms, 4342))
 
-    e = green(orig_eid); r = red(orig_rloc); m = bold(orig_ms)
-    print("Map-Register sent to {} for EID {} RLOC {} with nonce {}".format( \
-        m, e, r, nonce))
+    e = green(orig_eid); r = red(orig_rloc); m = bold(orig_ms);
+    print("Map-Register sent to {} for EID {} RLOC {} with nonce {}". \
+          format(m, e, r, hex(nonce)))
 
     #
     # Cleanup resources.
@@ -139,30 +142,49 @@ def main():
 # | c +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 # |  \|                             Locator                           |
 # +-> +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 #
-def build_map_register(afe, eid, ml, afl, rloc, ms, ms_key):
+def build_map_register(afe, iid, eid, ml, afl, rloc, ms, ms_key):
     sl = socket.htonl
     ss = socket.htons
 
     #
-    # Build RLOC-record.
+    # Build RLOC-record. RLOC is an AFI-List LCAF so an RLOC address and
+    # name can be enocded.
     #
+    rloc_name = "liar-{}".format(socket.gethostname())
+    rloc_name = (rloc_name + "\0").encode()
     rloc_record = struct.pack("BBBB", 1, 100, 0, 0)
-    rloc_record += struct.pack("HH", 0, ss(afl))
+    rloc_record += struct.pack("HHHBB", 0, ss(LCAF_AFI), 0, 1, 0)
+
+    lcaf_len = 4 + 2 if (afl == IPV4_AFI) else 16 + 2
+    lcaf_len += len(rloc_name) + 2
+    rloc_record += struct.pack("H", ss(lcaf_len))
+
+    #
+    # Insert RLOC address first and then dist-name next.
+    #
+    rloc_record += struct.pack("H", ss(afl))
     if (afl == IPV4_AFI): rloc_record += struct.pack("I", sl(rloc))
     if (afl == IPV6_AFI):
         a1 = byte_swap_64(rloc >> 64)
         a2 = byte_swap_64(rloc & 0xffffffffffffffff)
         rloc_record += struct.pack("QQ", a1, a2)
     #endif
+    rloc_record += struct.pack("H", ss(NAME_AFI))
+    rloc_record += rloc_name
 
     #
     # Build EID-record. Append RLOC-recort to it.
     #
-    eid_record = struct.pack("IBBHHH", sl(60), 1, ml, 0, 0, ss(afe))
+    eid_record = struct.pack("IBBHHHH", sl(60), 1, ml, 0, 0, ss(LCAF_AFI), 0)
+    eid_record += struct.pack("BB", 2, 0)
+    lcaf_len = 4 + 2
+    lcaf_len += 4 if (afe == IPV4_AFI) else 16
+    eid_record += struct.pack("H", ss(lcaf_len))
+    eid_record += struct.pack("IH", sl(iid), ss(afe))
+
     if (afe == IPV4_AFI): eid_record += struct.pack("I", sl(eid))
-    if (afe == NAME_AFI): eid_record += eid + "\0"
+    if (afe == NAME_AFI): eid_record += (eid + "\0").encode()
     if (afe == IPV6_AFI):
         a1 = byte_swap_64(eid >> 64)
         a2 = byte_swap_64(eid & 0xffffffffffffffff)
@@ -171,30 +193,42 @@ def build_map_register(afe, eid, ml, afl, rloc, ms, ms_key):
     eid_record += rloc_record
 
     #
-    # Build rest of Map-Register and append EID-record to it.
+    # Build rest of Map-Register with zeroed auth-data and append EID-record
+    # to it.
     #
     pkt = struct.pack("I", sl(0x30000001))
-
     nonce = random.randint(0, 0xffffffffffffffff)
-    pkt += struct.pack("QBBHQQQQ", nonce, 0, 2, ss(32), 0, 0, 0, 0)
+    pkt += struct.pack("QBBH", nonce, 0, 2, ss(32))
+    pkt += struct.pack("QQQQ", 0, 0, 0, 0)
     pkt += eid_record
 
     #
-    # Compute authentication hash across entire packet using ms_key.
+    # Compute authentication hash across entire packet using ms_key. Use
+    # 32-byte produced sha256() hash.
     #
+    hashval = hmac.new(ms_key.encode(), pkt, hashlib.sha256).digest()
+    pkt = pkt[0:16] + hashval + pkt[16+32::]
 
-    #
-    # Build UDP header. Not going to checksum right now.
-    #
-    sport = random.randint(0xf000, 0xffff)
-    length = len(pkt)
-    udp = struct.packet("HHHH", ss(sport), ss(4342), ss(length), 0)
-
-    #
-    # Return Map-Register message inside UDP.
-    #
-    pkt = udp + pkt
+#   print_packet(pkt)
     return(pkt, nonce)
+#enddef
+
+#
+# print_packet
+#
+# For looking at packet hex.
+#
+def print_packet(packet):
+    packet = binascii.hexlify(packet)
+    offset = 0
+    new = b""
+    length = len(packet) * 2
+    while (offset < length):
+        new += packet[offset:offset+8] + b" "
+        offset += 8
+        length -= 4
+    #endfor
+    print(new.decode())
 #enddef
 
 #
@@ -204,25 +238,34 @@ def build_map_register(afe, eid, ml, afl, rloc, ms, ms_key):
 # or distinguished-names "<string>".
 #
 def validate_eid(eid):
-    try:
-        eid, ml = eid.split("/")
-    except:
-        print("EID must be in prefix slash format")
-        return(None, None, None)
-    #endtry
+
+    #
+    # Parse instance-id first. If not supplied, iid is 0.
+    #
+    iid = 0
+    i = eid.find("["); j = eid.find("]")
+    if (i != -1 and j != -1): 
+        iid = int(eid[i+1:j])
+        eid = eid[j+1::]
+    #endif
+    
+    e = eid.split("/")
+    ml = int(e[1]) if len(e) == 2 else None
+    eid = e[0]
 
     #
     # Check for IPv4.
     #
     if (eid.count(".") == 3):
         try:
-            eid = socket.inet_pton(socket.AF_INET6, eid)
+            eid = socket.inet_pton(socket.AF_INET, eid)
         except:
             print("Invalid IPv4 EID address format")
-            return(None, None, None)
+            return(None, None, None, None)
         #endtry
         eid = int(binascii.hexlify(eid), 16)
-        return(IPV4_AFI, eid, ml)
+        if (ml == None): ml = 32
+        return(IPV4_AFI, iid, eid, ml)
     #endif
 
     #
@@ -233,16 +276,22 @@ def validate_eid(eid):
             eid = socket.inet_pton(socket.AF_INET6, eid)
         except:
             print("Invalid IPv6 EID address format")
-            return(None, None, None)
+            return(None, None, None, None)
         #endtry
         eid = int(binascii.hexlify(eid), 16)
-        return(IPV6_AFI, socket.htonl(eid), ml)
+        if (ml == None): ml = 128
+        return(IPV6_AFI, iid, eid, ml)
+    #endif
+
+    if (eid.count(".") > 3):
+        print("Invalid distinguished-name EID format")
+        return(None, None, None, None)
     #endif
 
     #
     # Must be a distinguished-name.
     #
-    return(NAME_AFI, eid, len(eid) * 8)
+    return(NAME_AFI, iid, eid, len(eid) * 8)
 #enddef
 
 #
@@ -257,10 +306,10 @@ def validate_rloc(rloc):
     #
     if (rloc.count(".") == 3):
         try:
-           rloc = socket.inet_pton(socket.AF_INET6, rloc)
+           rloc = socket.inet_pton(socket.AF_INET, rloc)
         except:
             print("Invalid IPv4 RLOC address format")
-            return(None, None, None)
+            return(None, None)
         #endtry
         rloc = int(binascii.hexlify(rloc), 16)
         return(IPV4_AFI, rloc)
