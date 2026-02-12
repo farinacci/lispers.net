8e9c5f41b18113ecb91b66781b78e96289cff632
Dino Farinacci <farinacci@gmail.com>
2026-02-12 15:29:30 -0800

    Turn off RLOC memory address display in lisp-mc.py.

--- lisp/lisp-mc.py
+++ lisp/lisp-mc.py
@@ -37,7 +37,7 @@ import json
 #
 # For turning on map-cache and rloc memory address debugging.
 #
-debug = True
+debug = False
 
 #------------------------------------------------------------------------------
 
